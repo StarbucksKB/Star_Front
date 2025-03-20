6afed18104f1e9e9ebab4d7f9c9ad6daa02b7a5e
wmh6525 <wmh6525@naver.com>
2025-03-20 17:47:20 +0900

    [feat] 하고자 했으나 아직 구현을 못함...

--- Back_node/index.js
+++ Back_node/index.js
@@ -24,84 +24,42 @@ app.get('/product', (req, res) => {
       prodName: '나이트로 바닐라 크림 라떼',
       prodPrice: 1500,
       img: '/Image/drink1.png',
-      Kcal: 400,
-      transfat: 22,
-      protein: 8,
-      sodium: 140,
-      sugar: 30,
-      caffeine: 105,
     },
     {
       prodNo: 'C0002',
       prodName: '리저브 HOT라떼',
       prodPrice: 1500,
       img: '/Image/drink2.jpg',
-      Kcal: 400,
-      transfat: 22,
-      protein: 8,
-      sodium: 140,
-      sugar: 30,
-      caffeine: 105,
     },
     {
       prodNo: 'C0003',
       prodName: 'HOT아메리카노',
       prodPrice: 2000,
       img: '/Image/drink3.jpg',
-      Kcal: 400,
-      transfat: 22,
-      protein: 8,
-      sodium: 140,
-      sugar: 30,
-      caffeine: 105,
     },
     {
       prodNo: 'C0004',
       prodName: 'HOT카페 라테',
       prodPrice: 2000,
       img: '/Image/drink4.jpg',
-      Kcal: 400,
-      transfat: 22,
-      protein: 8,
-      sodium: 140,
-      sugar: 30,
-      caffeine: 105,
     },
     {
       prodNo: 'C0005',
       prodName: '콜드브루몰트',
       prodPrice: 2500,
       img: '/Image/drink5.jpg',
-      Kcal: 400,
-      transfat: 22,
-      protein: 8,
-      sodium: 140,
-      sugar: 30,
-      caffeine: 105,
     },
     {
       prodNo: 'C0006',
       prodName: '카페브레베',
       prodPrice: 3500,
       img: '/Image/drink6.jpg',
-      Kcal: 400,
-      transfat: 22,
-      protein: 8,
-      sodium: 140,
-      sugar: 30,
-      caffeine: 105,
     },
     {
       prodNo: 'C0007',
       prodName: '바닐라라떼',
       prodPrice: 3500,
       img: '/Image/drink7.jpg',
-      Kcal: 400,
-      transfat: 22,
-      protein: 8,
-      sodium: 140,
-      sugar: 30,
-      caffeine: 105,
     },
   ];
   res.json(products);
@@ -111,19 +69,82 @@ app.get('/product/:prodNo', (req, res) => {
   // res.send(`${req.params.prodNo}상품의 상세내용입니다`);
   let product;
   if (req.params.prodNo == 'C0001') {
-    product = { prodName: '아메리카노', img: 'C0001.jpg' };
+    product = {
+      prodName: '아메리카노',
+      img: 'C0001.jpg',
+      Kcal: 400,
+      transfat: 22,
+      protein: 8,
+      sodium: 140,
+      sugar: 30,
+      caffeine: 105,
+    };
   } else if (req.params.prodNo == 'C0002') {
-    product = { prodName: '아이스아메리카노', img: 'C0002.jpg' };
+    product = {
+      prodName: '아이스아메리카노',
+      img: 'C0002.jpg',
+      Kcal: 400,
+      transfat: 22,
+      protein: 8,
+      sodium: 140,
+      sugar: 30,
+      caffeine: 105,
+    };
   } else if (req.params.prodNo == 'C0003') {
-    product = { prodName: '라테', img: 'C0003.jpg' };
+    product = {
+      prodName: '라테',
+      img: 'C0003.jpg',
+      Kcal: 400,
+      transfat: 22,
+      protein: 8,
+      sodium: 140,
+      sugar: 30,
+      caffeine: 105,
+    };
   } else if (req.params.prodNo == 'C0004') {
-    product = { prodName: '아이스라테', img: 'C0004.jpg' };
+    product = {
+      prodName: '아이스라테',
+      img: 'C0004.jpg',
+      Kcal: 400,
+      transfat: 22,
+      protein: 8,
+      sodium: 140,
+      sugar: 30,
+      caffeine: 105,
+    };
   } else if (req.params.prodNo == 'C0005') {
-    product = { prodName: '콜드브루몰트', img: 'C0005.jpg' };
+    product = {
+      prodName: '콜드브루몰트',
+      img: 'C0005.jpg',
+      Kcal: 400,
+      transfat: 22,
+      protein: 8,
+      sodium: 140,
+      sugar: 30,
+      caffeine: 105,
+    };
   } else if (req.params.prodNo == 'C0006') {
-    product = { prodName: '카페브레베', img: 'C0006.jpg' };
+    product = {
+      prodName: '카페브레베',
+      img: 'C0006.jpg',
+      Kcal: 400,
+      transfat: 22,
+      protein: 8,
+      sodium: 140,
+      sugar: 30,
+      caffeine: 105,
+    };
   } else if (req.params.prodNo == 'C0007') {
-    product = { prodName: '바닐라라떼', img: 'C0007.jpg' };
+    product = {
+      prodName: '바닐라라떼',
+      img: 'C0007.jpg',
+      Kcal: 400,
+      transfat: 22,
+      protein: 8,
+      sodium: 140,
+      sugar: 30,
+      caffeine: 105,
+    };
   } else {
     product = {};
   }
