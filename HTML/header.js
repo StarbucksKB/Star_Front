06f5ffc0054e739d94453a2429189ffddc688008
yebinnnnn <prettybird1@naver.com>
2025-03-20 10:29:02 +0900

    FEAT: 헤더 적용 완료!

--- HTML/header.js
+++ HTML/header.js
@@ -1,27 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("/HTML/header.html")
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('/HTML/header.html')
     .then((res) => res.text())
     .then((headerHTML) => {
-      document.getElementById("header").innerHTML = headerHTML;
+      document.getElementById('header').innerHTML = headerHTML;
 
-      document.querySelector(".productList").onclick = () => {
-        location.href = "/HTML/Products/productList.html";
+      document.querySelector('#imagelogo').onclick = () => {
+        location.href = 'HTML/main.html';
       };
-      document.querySelector(".cartList").onclick = () => {
-        location.href = "/HTML/Products/cartList.html";
+      document.querySelector('.productList').onclick = () => {
+        location.href = '/HTML/Products/productList.html';
       };
-      document.querySelector(".orderList").onclick = () => {
-        location.href = "/HTML/Products/orderList.html";
+      document.querySelector('.cartList').onclick = () => {
+        location.href = '/HTML/Products/cartList.html';
       };
-      document.querySelector(".login").onclick = () => {
-        location.href = "/HTML/Members/login.html";
+      document.querySelector('.orderList').onclick = () => {
+        location.href = '/HTML/Products/orderList.html';
       };
-      document.querySelector(".join").onclick = () => {
-        location.href = "/HTML/Members/join.html";
+      document.querySelector('.login').onclick = () => {
+        location.href = '/HTML/Members/login.html';
       };
-      document.querySelector(".logout").onclick = () => {
-        alert("로그아웃 되셨습니다.");
+      document.querySelector('.join').onclick = () => {
+        location.href = '/HTML/Members/join.html';
+      };
+      document.querySelector('.logout').onclick = () => {
+        alert('로그아웃 되셨습니다.');
       };
     })
-    .catch((err) => console.error("헤더 로딩 중 오류 발생:", err));
+    .catch((err) => console.error('헤더 로딩 중 오류 발생:', err));
 });
